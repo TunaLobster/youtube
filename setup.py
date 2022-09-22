1f7449f9c2273d406b38ed1b0ae887464cb65560
TunaLobster <charliepjohnson@gmail.com>
2022-09-22 09:54:18 -0500

    Fix setup.py for requests requirement

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 #-*- coding:utf-8 -*-
 
 import setuptools
-from sphinxcontrib import youtube
 
 
 setuptools.setup(
     name='sphinxcontrib.youtube',
-    version=youtube.__version__,
+    version='1.2.0',
     packages=setuptools.find_packages(),
     install_requires=[
-        'sphinx'
+        'sphinx',
+        'requests',
         ],
     url='https://github.com/ArduPilot/sphinxcontrib-youtube',
     description='''embedding youtube videos into sphinx''',
-    long_description=youtube.__doc__,
     namespace_packages=['sphinxcontrib'],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
